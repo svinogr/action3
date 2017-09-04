4fdc549832ca6842140976a369a7f733d0920917
svinogr <svinogr@hotmail.com>
2017-09-04 23:37:53 +0300

    add  controller

---
+++ src/main/java/ap/controllers/AccountController.java
@@ -0,0 +1,10 @@
+package ap.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class AccountController {
+
+}
